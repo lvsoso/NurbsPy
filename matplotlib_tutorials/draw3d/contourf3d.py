b2659dd8e8c43b664d3949e6f13e304df1761490
周勇 <m18343381753@163.com>
2018-03-01 23:13:10 +0800

    matplotlib 3d draw figure

---
+++ matplotlib_tutorials/draw3d/contourf3d.py
@@ -0,0 +1,19 @@
+#!coding:UTF-8
+'''
+Created on 2018年3月1日
+
+@author: m1834
+'''
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+X, Y, Z = axes3d.get_test_data(0.05)
+
+cset = ax.contourf(X, Y, Z, cmap=cm.coolwarm)
+
+ax.clabel(cset, fontsize=9, inline=1)
+
+plt.show()
